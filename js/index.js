c8a72bd247d6c89ebd524b017bec07e6ad0dd77f
Myhailo Deba <darklord793@gmail.com>
2018-11-23 16:39:02 +0000

    initial

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ function fibonacciСycle(a) {
     let first = 1;
     let second = 1;
     if (a === 1) {
-        return "Under the number a :" + a + ", in the Fibonacci lsine is the number :" + first;
+        return "Under the number a :" + a + ", in the Fibonacci lsine is the number : " + first;
     } else if (a === 2) { 
-        return "Under the number a :" + a + ", in the Fibonacci line is the number :" + second;
+        return "Under the number a :" + a + ", in the Fibonacci line is the number : " + second;
     } else {
     for (let i = 3; i <=a; i++) {
     let next = first + second;
@@ -23,13 +23,13 @@ const b = prompt('Enter position number in the Fibonacci line. To calculate in M
 
     let fibonacciMassif= [0, 1, 1,];
     if (b === 1) {
-        alert "Under the number b :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[1];
+        alert("Under the number in the Fibonacci line is the number : " + fibonacciMassif[1]);
     } else if (b === 2) { 
-        alert "Under the number b :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[2];
+        alert("Under the number in the Fibonacci line is the number : " + fibonacciMassif[2]);
     } else {
     for (i = 3; i <= b; i++) {
     fibonacciMassif[i].push (fibonacciMassif[i - 1] + fibonacciMassif[i - 2]);
-        alert "Under the number b :" + b + ", in the Fibonacci line is the number :" + fibonacciMassif[i];
+        alert("Under the number in the Fibonacci line is the number : " + fibonacciMassif[i]);
     }
 
 let resultMassif = fibonacciMassif(b);
